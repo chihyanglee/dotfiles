a07dfa959b193aacb430aa2ac48ab9042941a12d
chihyanglee <chihyanglee@gmail.com>
2026-03-08 10:30:51 +0800

    add fcitx5 input method setup

---
+++ config/eww/scripts/im.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+im="$(fcitx5-remote -n 2>/dev/null)"
+
+case "$im" in
+  keyboard-us)  echo "EN" ;;
+  rime)         echo "中" ;;
+  mozc)         echo "あ" ;;
+  *)            echo "??" ;;
+esac
